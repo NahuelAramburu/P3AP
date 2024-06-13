087d610778ee6bc68818133041dd0dccc86fa748
Juliana Irungaray <julianairungaray3@gmail.com>
2024-06-13 14:51:04 -0300

    86a3p6br8-animaciones para la transición de la puerta y responsive en MainContent y Door

--- app/src/layouts/MainContent/MainContent.js
+++ app/src/layouts/MainContent/MainContent.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import CastleImage from '../../assets/images/castle1.jpeg';
+import Door from '../../components/Door/Door';
 import './MainContent.css'; 
 
 const MainContent = () => {
+  const [isDoorVisible, setIsDoorVisible] = useState(true);
+
+  const handleDoorClose = () => {
+    setIsDoorVisible(false);
+  }
+
   return (
-      <div className="main-content" style={{ backgroundImage: `url(${CastleImage})`, backgroundPosition: 'center' }}>
-        <a href="/page1" className="link-window">Page 1</a>
-        <a href="/page2" className="link-door">Page 2</a>
-        <a href="/page3" className="link-window">Page 3</a>
-        <a href="/page4" className="link-door">Page 4</a>
-      </div>
+    <div className="main-content" style={{ backgroundImage: `url(${CastleImage})`, backgroundPosition: 'center' }}>
+      {isDoorVisible ? (
+        <Door onClose={handleDoorClose} />
+      ) : (
+        <>
+          <a href="/page1" className="link-window">Page 1</a>
+          <a href="/page2" className="link-door">Page 2</a>
+          <a href="/page3" className="link-window">Page 3</a>
+          <a href="/page4" className="link-door">Page 4</a>
+        </>
+      )}
+    </div>
   );
 };
 
 export default MainContent;
-
