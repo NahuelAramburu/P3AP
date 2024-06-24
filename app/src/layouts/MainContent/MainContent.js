940a1fedc8fa3a6843650b67847b4092eba849dc
Juliana Irungaray <julianairungaray3@gmail.com>
2024-06-24 11:14:30 -0300

    se añadió un array para mostrar cada obra y se agregó componente Link de react-router-dom

--- app/src/layouts/MainContent/MainContent.js
+++ app/src/layouts/MainContent/MainContent.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import CastleImage from '../../assets/images/castle1.jpeg';
 import Door from '../../components/Door/Door';
 import './MainContent.css'; 
 
 const MainContent = () => {
   const [isDoorVisible, setIsDoorVisible] = useState(true);
+  const [activeLink, setActiveLink] = useState('');
 
   const handleDoorClose = () => {
     setIsDoorVisible(false);
+  }
+
+  const handleClick = (linkName) => {
+    setActiveLink(linkName);
+    setTimeout(() => setActiveLink(''), 2000); // Remueve la clase después de que la animación se complete
   }
 
   return (
@@ -15,15 +22,55 @@ const MainContent = () => {
       {isDoorVisible ? (
         <Door onClose={handleDoorClose} />
       ) : (
-        <>
-          <a href="/page1" className="link-window">Page 1</a>
-          <a href="/page2" className="link-door">Page 2</a>
-          <a href="/page3" className="link-window">Page 3</a>
-          <a href="/page4" className="link-door">Page 4</a>
-        </>
+        <div className="navigation-links-container">
+          <Link to="/page1" className={`navigation-link ${activeLink === 'page1' ? 'zoom-out' : ''}`} onClick={() => handleClick('page1')}>Artista</Link>
+          <Link to="/page2" className={`navigation-link ${activeLink === 'page2' ? 'zoom-out' : ''}`} onClick={() => handleClick('page2')}>Historia</Link>
+          <Link to="/page3" className={`navigation-link ${activeLink === 'page3' ? 'zoom-out' : ''}`} onClick={() => handleClick('page3')}>Obras</Link>
+          <Link to="/page4" className={`navigation-link ${activeLink === 'page4' ? 'zoom-out' : ''}`} onClick={() => handleClick('page4')}>Zona Exclusiva</Link>
+        </div>
       )}
     </div>
   );
 };
 
 export default MainContent;
+
+// import { useState, useEffect } from 'react';
+// import CastleImage from '../../assets/images/castle1.jpeg';
+// import Door from '../../components/Door/Door';
+// import './MainContent.css'; 
+
+// const MainContent = () => {
+//   const [isDoorVisible, setIsDoorVisible] = useState(false);
+
+//   useEffect(() => {
+//     const isFirstVisit = localStorage.getItem('isFirstVisit');
+//     if (!isFirstVisit) {
+//       setIsDoorVisible(true);
+//       localStorage.setItem('isFirstVisit', 'no');
+//     }
+//   }, []);
+
+//   const handleDoorClose = () => {
+//     setIsDoorVisible(false);
+//   }
+
+//   return (
+//     <div className="main-content" style={{ backgroundImage: `url(${CastleImage})`, backgroundPosition: 'center' }}>
+//       {isDoorVisible ? (
+//         <Door onClose={handleDoorClose} />
+//       ) : (
+//         <div className="navigation-links-container">
+//           <a href="/page1" className="navigation-link">Artista</a>
+//           <a href="/page2" className="navigation-link">Historia</a>
+//           <a href="/page3" className="navigation-link">Obras</a>
+//           <a href="/page4" className="navigation-link">Zona Exclusiva</a>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MainContent;
+
+
