c347e6b693edf7162165a1e6233439b8c4e92aa1
Juliana Irungaray <julianairungaray3@gmail.com>
2024-06-25 15:36:42 -0300

    86a3p6br8-Video introductorio provisional con animación de puerta para la entrada a la página principal

--- app/src/components/Door/Door.js
+++ app/src/components/Door/Door.js
@@ -1,11 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Door.css'; 
+import doorVideo from '../../assets/images/video.mp4'; 
 
-const Door = ({ onClose }) => {
+  const Door = ({ onClose }) => {
   const [isVisible, setIsVisible] = useState(true);
+  const videoRef = useRef(null); 
 
   const handleClick = () => {
     setIsVisible(false);
+
+
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
 
     setTimeout(() => {
       onClose();
@@ -13,7 +20,11 @@ const Door = ({ onClose }) => {
   };
 
   return (
-    <div className={`door ${isVisible ? '' : 'fade-out'}`} onClick={handleClick}></div>
+    <div className={`door ${isVisible ? '' : 'fade-out'}`} onClick={handleClick}>
+      <video ref={videoRef} loop muted className="door-video" style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' }}>
+        <source src={doorVideo} type="video/mp4" />
+      </video>
+    </div>
   );
 };
 
