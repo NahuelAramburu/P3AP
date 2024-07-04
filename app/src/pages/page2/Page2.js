829fe8bb1260bbde656866ce38f46d136b62861a
AgustinR22 <agusdev222@gmail.com>
2024-07-04 17:50:04 -0300

    cambios

--- app/src/pages/page2/Page2.js
+++ app/src/pages/page2/Page2.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Page2Image from '../../assets/images/pag2.jpeg';
+import Page2Frame from '../../assets/images/frame1.png';
+import cartel from '../../assets/images/back.png';
 import './Page2.css';
+import { Link } from 'react-router-dom';
 
 const Page2 = () => {
   return (
     <div className="page-content" style={{ backgroundImage: `url(${Page2Image})`, backgroundPosition: 'center' }}>
     
+      <Link to ="/">
+        <img src={cartel} className="cartel-image" alt="Cartel" />
+      </Link>
+
+      <h2 className='titulo'>Leonardo Vladimir (Leovlad R. T. F)</h2>
+
+      <div className='text-container'>
+        <p className='texto'>Nació en la Republica Argentina. Ha estudiado con importantes profesores de su natal ciudad de Ensenada, Ciudad de La Plata, y Ciudad de Buenos Aires, y desde su tierna niñez ha tomado el pincel hasta el dia de hoy. La Escuela de Bellas Artes de la Universidad Nacional de La Plata ha sido su lugar de estudios principal, escuela que tantos conocimientos le ha aportado. Ha expuesto y participado en numerosas muestras y concursos a lo largo de toda su carrera. Él mismo se autodenomina como un "Autor Libre y Creativo".
+        </p>
+        <div className='frame-container'>
+          <img className='pintura' src={Page2Image} alt='Pintura'></img>
+          <img className='marco' src={Page2Frame} alt='Marco'></img>
+        </div>
+      </div>
+
     </div>
   );
 };
