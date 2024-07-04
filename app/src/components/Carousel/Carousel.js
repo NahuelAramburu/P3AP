ffd24621c8a881d6f2c63e78b478088c07863999
Juliana Irungaray <julianairungaray3@gmail.com>
2024-07-04 16:15:48 -0300

    Se actualizo Carousel para cargar datos de obras desde un archivo JSON local

--- app/src/components/Carousel/Carousel.js
+++ app/src/components/Carousel/Carousel.js
@@ -1,82 +1,39 @@
-// import React, { useState } from 'react';
-// import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
-// import img1 from '../../assets/images/img1.jpg';
-// import img2 from '../../assets/images/img2.jpg';
-// import img3 from '../../assets/images/img3.jpg';
-// import './Carousel.css';
-
-// const artworks = [
-//   { src: img1, alt: 'Obra de arte 1', nombre: 'Nombre 1', pais: 'País 1', tematica: 'Temática 1', precio: 'Precio 1' },
-//   { src: img2, alt: 'Obra de arte 2', nombre: 'Nombre 2', pais: 'País 2', tematica: 'Temática 2', precio: 'Precio 2' },
-//   { src: img3, alt: 'Obra de arte 3', nombre: 'Nombre 3', pais: 'País 3', tematica: 'Temática 3', precio: 'Precio 3' },
-// ];
-
-// const Carousel = () => {
-//   const [current, setCurrent] = useState(0);
-  
-//   const nextSlide = () => {
-//     setCurrent(current === artworks.length - 1 ? 0 : current + 1);
-//   };
-
-//   const prevSlide = () => {
-//     setCurrent(current === 0 ? artworks.length - 1 : current - 1);
-//   };
-
-//   return (
-//     <div className="carousel">
-//       <div className="art-description">
-//         <h2>{artworks[current].nombre}</h2>
-//         <p><strong>País:</strong> {artworks[current].pais}</p>
-//         <p><strong>Temática:</strong> {artworks[current].tematica}</p>
-//         <p><strong>Precio: $</strong>{artworks[current].precio}</p>
-//       </div>
-//       <div className="slider">
-//         {artworks.map((art, index) => (
-//           <div className={index === current ? 'slide active' : 'slide'} key={index}>
-//             {index === current && (
-//               <div className="image-container">
-//                 <img src={art.src} alt={art.alt} className="image" />
-//               </div>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//       <button className="left-arrow" onClick={prevSlide}>
-//         <FaArrowAltCircleLeft />
-//       </button>
-//       <button className="right-arrow" onClick={nextSlide}>
-//         <FaArrowAltCircleRight />
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Carousel;
-
-
 import React, { useState, useEffect } from 'react';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
-import img1 from '../../assets/images/img1.jpg';
-import img2 from '../../assets/images/img2.jpg';
-import img3 from '../../assets/images/img3.jpg';
 import './Carousel.css';
 
-const artworks = [
-  { src: img1, alt: 'Obra de arte 1', nombre: 'Nombre 1', pais: 'País 1', tematica: 'Temática 1', precio: 'Precio 1' },
-  { src: img2, alt: 'Obra de arte 2', nombre: 'Nombre 2', pais: 'País 2', tematica: 'Temática 2', precio: 'Precio 2' },
-  { src: img3, alt: 'Obra de arte 3', nombre: 'Nombre 3', pais: 'País 3', tematica: 'Temática 3', precio: 'Precio 3' },
-];
-
 const Carousel = () => {
+  const [obras, setObras] = useState([]);
   const [current, setCurrent] = useState(0);
   const [popupVisible, setPopupVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentImage, setCurrentImage] = useState('');
+
+  useEffect(() => {
+    const fetchObras = async () => {
+      try {
+        const response = await fetch('/obras.json'); 
+        const data = await response.json();
+        setObras(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchObras();
+  }, []);
 
   const nextSlide = () => {
-    setCurrent(current === artworks.length - 1 ? 0 : current + 1);
+    setCurrent(current === obras.length - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? artworks.length - 1 : current - 1);
+    setCurrent(current === 0 ? obras.length - 1 : current - 1);
+  };
+
+  const openModal = (imageSrc) => {
+    setCurrentImage(imageSrc);
+    setModalVisible(true);
   };
 
   useEffect(() => {
@@ -102,33 +59,47 @@ const Carousel = () => {
 
   return (
     <div className="carousel">
-      <div className="art-description">
-        <h2>{artworks[current].nombre}</h2>
-        <p><strong>País:</strong> {artworks[current].pais}</p>
-        <p><strong>Temática:</strong> {artworks[current].tematica}</p>
-        <p><strong>Precio: $</strong>{artworks[current].precio}</p>
-      </div>
-      <div className="slider">
-        {artworks.map((art, index) => (
-          <div className={index === current ? 'slide active' : 'slide'} key={index}>
-            {index === current && (
-              <div className="image-container">
-                <img src={art.src} alt={art.alt} className="image" />
-              </div>
-            )}
+      {obras.length > 0 && (
+        <>
+          <div className="art-description">
+            <h2>{obras[current].nombre}</h2>
+            <p><strong>País:</strong> {obras[current].pais}</p>
+            <p><strong>Temática:</strong> {obras[current].tematica}</p>
+            <p><strong>Técnica y soportes:</strong> {obras[current].tecnica}, {obras[current].soportes}</p>
+            <p><strong>Medidas:</strong> {obras[current].medidas}</p>
+            <p><strong>En Artelista desde:</strong> {obras[current].enArtelistaDesde}</p>
+            <p><strong>Descripción:</strong> {obras[current].descripcion}</p>
+            <p><strong>Precio: $</strong>{obras[current].precio}</p>
           </div>
-        ))}
-      </div>
-      <button className="left-arrow" onClick={prevSlide}>
-        <FaArrowAltCircleLeft />
-      </button>
-      <button className="right-arrow" onClick={nextSlide}>
-        <FaArrowAltCircleRight />
-      </button>
+          <div className="slider">
+            {obras.map((art, index) => (
+              <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                {index === current && (
+                  <div className="image-container" onClick={() => openModal(art.src)}>
+                    <img src={art.src} alt={art.alt} className="image" />
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <button className="left-arrow" onClick={prevSlide}>
+            <FaArrowAltCircleLeft />
+          </button>
+          <button className="right-arrow" onClick={nextSlide}>
+            <FaArrowAltCircleRight />
+          </button>
+        </>
+      )}
       {popupVisible && (
         <div className="popup">
           <p>Acción no permitida</p>
           <button onClick={() => setPopupVisible(false)}>Cerrar</button>
+        </div>
+      )}
+      {modalVisible && (
+        <div className="modal">
+          <span className="close-modal" onClick={() => setModalVisible(false)}>&times;</span>
+          <img src={currentImage} alt="Ampliada" className="modal-content" />
         </div>
       )}
     </div>
